5c51f3dd7595048c96f1fe74d89d5aff1f05c6e0
ghiffariii <3337230071@untirta.ac.id>
2023-09-27 00:26:40 +0700

    Tes

--- AlPro/pertemuan4.cpp
+++ AlPro/pertemuan4.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
-int main () {
-    cout << "Helloworld!" << endl;
+int main (int argc, char *argv[]) {
+  void percabangan_1 (); {
+    int nilai;
+
+    cout << "Masukkan nilai: "; cin >> nilai;
+
+    if (nilai >= 90) {
+        cout << "Nilai angka: A" << endl;
+    } if (nilai < 90 && nilai > 60) {
+        cout << "Nilai angka: B" << endl;
+    } else {
+        cout << "Maaf anda tidak lulus" << endl;
+    }
+
+  }
+
+  // void percabangan_1
 }
