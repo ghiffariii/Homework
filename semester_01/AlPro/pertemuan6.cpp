8d51caedf495c2e9cf0029a506665432936aedc5
ramadhafidz <hrghiffari@gmail.com>
2023-11-03 23:12:45 +0700

    .

--- semester_01/AlPro/pertemuan6.cpp
+++ semester_01/AlPro/pertemuan6.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include<vector>
-#include<cmath>
-#include<cstdlib>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -14,56 +14,56 @@ void array1() {
   data[3]=5;
   data[4]=8;
 
-  cout<<data[3]<<endl;
+  cout << data[3] << endl;
 }
 
 void array2() {
   char data[5];
 
-  data[0]='a';
-  data[1]='v';
-  data[2]='b';
-  data[3]='f';
-  data[4]='r';
+  data[0] = 'a';
+  data[1] = 'v';
+  data[2] = 'b';
+  data[3] = 'f';
+  data[4] = 'r';
 
-  cout<<data[3]<< endl;
+  cout << data[3] << endl;
 }
 
 void array3() {
   string data[5];
 
-  data[0]= "data1";
-  data[1]="data2";
-  data[2]="data3";
-  data[3]="data4";
-  data[4]="data5";
+  data[0] = "data1";
+  data[1] = "data2";
+  data[2] = "data3";
+  data[3] = "data4";
+  data[4] = "data5";
 
-  cout<<data[3]<< endl;
+  cout << data[3] << endl;
 }
 
 void array4() {
   int data[10];
   
-  for(int i=0; i<10; i++) {
-    data[i]= rand();
-    cout<<"data ke "<<i+1<< " adalah "<< data[i]<<endl;
+  for(int i = 0; i < 10; i++) {
+    data[i] = rand();
+    cout << "data ke " << i + 1 << " adalah " << data[i] <<endl;
   }
 }
 
 void array5() {
   int n;
 
-  cout<<"masukan banyak data: "; cin>>n;
+  cout << "masukan banyak data: "; cin >> n;
 
   int data[n];
 
-  for(int i=0; i<n; i++){
-    cout<<"data ke: "<<i<<" adalah "; cin>>data[i];
+  for(int i = 0; i < n; i++) {
+    cout << "data ke: " << i << " adalah "; cin >> data[i];
   }
-  cout<<endl;
+  cout << endl;
 
-  for(int i=0; i<n; i++){
-    cout<<"data ke: "<<i+1<<" adalah "<<data[i]<<endl;
+  for(int i = 0; i < n; i++) {
+    cout << "data ke: " << i + 1 << " adalah " << data[i] << endl;
   }
 }
 
